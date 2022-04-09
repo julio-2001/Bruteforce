965840b6d21ee447fbaef1d1307151c5aac9bd19
global <global>
2022-04-09 15:21:25 -0300

    wordlist

---
+++ wordlist.py
@@ -0,0 +1,85 @@
+import time as t
+from itertools import product
+
+
+
+""""
+
+                    -world  list-
+            
+
+
+"""
+
+ban = '''
+                                                    '''
+
+
+scale = input(" \033[32m[*]  Digite o tamanho da scala [EX 1 ate 8= [1:8]:  ")
+comeco = int(scale.split(':')[0])
+fim = int(scale.split(':')[1])
+
+t.sleep(0.5)
+
+
+info_pessoais = str(input("\n \033[32m[*] Deseja colocar informações pessoais? [s ou n]  :  "))
+if info_pessoais == 's':
+    primeiro_nome = str(input("\n \033[36m[*]Primeiro nome:  "))
+    sobrenome = str(input("\n \033[36m[*] Sobrenome:  "))
+    aniversario = str(input("\n \033[36m[*] Data do aniversario:   "))
+    mes = str(input("\n \033[36m[*] Digite o mês:   "))
+    ano = str(input("\n \033[36m[*]Digite o ano:  "))
+    chars = primeiro_nome + sobrenome + aniversario + mes + ano
+
+
+
+else:
+    chars = 'abcdefghijklmnopqrstuvwxyz'
+    pass
+
+
+chars_maiosculo = chars.upper()
+chars_numero = '0123456789'
+chars_especiais = '!\][/?.,~-="; :><@#$%&*()_+ \ ' 
+
+t.sleep(0.5)
+
+arquivo_nome = input("\n \033[36m[!] Digite o nome para seu arquivo de lista de palavras!:   ")
+arquivo = open(arquivo_nome, 'w')
+
+if input("\n Apenas numeros? [s ou n]:  ") == 's':
+    chars = chars_numero
+else:
+    if input("\n \033[36m[?] Deseja usar caracteres maiosculos? [s ou n]:   ") == 's':
+        chars = '' .join([chars , chars_maiosculo])
+
+    if input("\n \033[36m[?] Deseja usar caracteres especiais? [s ou n ]:   ") == 's':
+        chars = '' .join([chars, chars_especiais ])
+
+    if input("\n \033[36m[?] Deseja usar numeros? [s ou n ]:   ") == 's':
+        chars = ''.join([chars ,chars_numero ])
+
+
+try:
+    for i in range(comeco , fim +1):
+
+        cont = 0
+        
+
+        for j in product(chars, repeat=i):
+            tempo = t.strftime("%H:%M:%S")
+
+
+            temp = '' .join(j)
+            print(f" \033[36m <<<==========   \033[32m[  {temp}  ]\033[m  \033[36m ==========>>> \033[m  \033[30;43;1m {cont} \033[m   \033[34;42;1m {tempo} \033[m  ")
+            arquivo.write(temp + ' \n')
+            cont += 1
+            
+
+except(KeyboardInterrupt):
+    print(" \033[31m Usuario interrompeu o processo \033[m ") 
+
+
+
+    arquivo.close()
+  
