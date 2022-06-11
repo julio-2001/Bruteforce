ade56652b3ea3f4de6242c5d823c0aea97e74cc9
julio gabriel <santos2001martins@gmail.com>
2022-06-11 10:53:12 -0300

    melhoramento no visual add tentetivas por segundos , e total de tentativas

--- wordlist.py
+++ wordlist.py
@@ -1,5 +1,8 @@
+
+
+from ntpath import join
 import time as t
-from itertools import product
+from itertools import product  ,permutations as pms
 
 import socket as sk
 import socketserver as sks
@@ -9,24 +12,17 @@ import pandas as p
 import websocket as ws
 import requests as rts
 
-enreco_ip = sk.gethostname
-endereco_ip = sk.gethostname
 
 
 
 
 
-""""
-                    -world  list-
-            
-"""
+t.sleep(50)
+scale = input(" \033[32m[*]  Digite o tamanho da senha [EX 4 ate 8= [4:8]:  ")
 
-
-
-scale = input(" \033[32m[*]  Digite o tamanho da scala [EX 1 ate 8= [1:8]:  ")
 
 print(" \033[1;33;44m Para parar o processo digite [CTRL + C]! \033[m ")
-t.sleep(2)
+t.sleep(3)
 
 comeco = int(scale.split(':')[0])
 fim = int(scale.split(':')[1])
@@ -34,15 +30,34 @@ fim = int(scale.split(':')[1])
 t.sleep(0.5)
 
 
+
+
+
+
+
 info_pessoais = str(input("\n \033[32m[*] Deseja colocar informações pessoais? [s ou n]  :  "))
 if info_pessoais == 's':
-    primeiro_nome = str(input("\n \033[36m[*]Primeiro nome:  "))
-    sobrenome = str(input("\n \033[36m[*] Sobrenome:  "))
-    aniversario = str(input("\n \033[36m[*] Data do aniversario:   "))
-    mes = str(input("\n \033[36m[*] Digite o mês:   "))
-    ano = str(input("\n \033[36m[*]Digite o ano:  "))
-    chars = primeiro_nome + sobrenome + aniversario + mes + ano
 
+    primeiro_nome = str(input("\n \033[36m[*]Primeiro nome:  "))
+    if input("Possui outro nome? [s ou n]:   ")== 's':
+        global adicional_nome 
+        adicional_nome = input("digite o nome:   ")  
+        primeiro_nome = ''.join([primeiro_nome, adicional_nome])    
+       
+
+    sobrenome = str(input("\n \033[36m[*] Sobrenome:  "))
+
+
+    aniversario = str(input("\n \033[36m[*] Data do aniversario:   "))
+
+
+    mes = str(input("\n \033[36m[*] Digite o mês:   "))
+
+
+    ano = str(input("\n \033[36m[*]Digite o ano:  "))
+
+
+    chars = primeiro_nome + sobrenome + aniversario + mes + ano
 
 
 else:
@@ -59,6 +74,7 @@ t.sleep(0.5)
 arquivo_nome = input("\n \033[36m[!] Digite o nome para seu arquivo de lista de palavras!:   ")
 arquivo = open(arquivo_nome, 'w')
 
+
 if input("\n Apenas numeros? [s ou n]:  ") == 's':
     chars = chars_numero
 else:
@@ -67,6 +83,7 @@ else:
 
     if input("\n \033[36m[?] Deseja usar caracteres especiais? [s ou n ]:   ") == 's':
         chars = '' .join([chars, chars_especiais ])
+     
 
     if input("\n \033[36m[?] Deseja usar numeros? [s ou n ]:   ") == 's':
         chars = ''.join([chars ,chars_numero ])
@@ -80,16 +97,18 @@ try:
 
         for j in product(chars, repeat=i):
             tempo = t.strftime("%H:%M:%S")
+            CombinacoesSenhas = pms([comeco , chars.strip    ],comeco)
 
 
             temp = '' .join(j)
-            print(f" \033[36m <<<==========   \033[32m[  {temp}  ]\033[m  \033[36m ==========>>> \033[m  \033[30;43;1m {cont} \033[m   \033[34;42;1m {tempo} \033[m  ")
+            print(f" \033[36m <<<==========   \033[32m[  {temp}  ]\033[m  \033[36m ==========>>> \033[m  \033[30;43;1m {cont} \033[m   \033[34;42;1m {tempo} \033[m   {CombinacoesSenhas.__str__}  ")
             arquivo.write(temp + ' \n')
             cont += 1
             
 
 except(KeyboardInterrupt):
     print(" \033[31m Usuario interrompeu o processo \033[m ") 
+    t.sleep(1)
     print(f"Senhas registradas \033[1;32m {cont} \033[m  tempo termino \033[1;36m {tempo} \033[m  ")
     arquivo.close()
-  
+
